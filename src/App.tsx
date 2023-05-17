e02270d1303295301455632d13762a3b0660ae93
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-17 18:27:09 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
 import { useState, useEffect, ChangeEvent } from 'react'
-import { ref, uploadBytes, getDownloadURL, listAll } from 'firebase/storage'
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+} from 'firebase/storage'
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
+
 import { storage } from './config/firebase'
 import { v4 as uuidv4 } from 'uuid'
 
 function App(): JSX.Element {
   const [imageUpload, setImageUpload] = useState<File | null>(null)
   const [imageUrls, setImageUrls] = useState<string[]>([])
+  const [imageName, setImageName] = useState<string>('')
+  const [additionalData, setAdditionalData] = useState<string>('')
 
-  const imagesListRef = ref(storage, 'images/')
+  const imagesListRef = storageRef(storage, 'images/')
 
   const uploadFile = (): void => {
-    if (imageUpload === null) return
-    const imageName = imageUpload.name + uuidv4()
-    const imageRef = ref(storage, `images/${imageName}`)
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrls((prev) => [...prev, url])
+    if (
+      imageUpload === null ||
+      imageName.trim() === '' ||
+      additionalData.trim() === ''
+    )
+      return
+    const newImageName = imageName + uuidv4()
+    const imageRef = storageRef(storage, `images/${newImageName}`)
+
+    uploadBytes(imageRef, imageUpload)
+      .then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImageUrls((prev) => [...prev, url])
+        })
+
+        const imageData = {
+          name: imageName,
+          additionalData: additionalData,
+          url: snapshot.ref.fullPath,
+        }
+
+        const db = getFirestore()
+        const imagesCollection = collection(db, 'imagesData')
+
+        // Store additional data in Firestore
+        addDoc(imagesCollection, imageData)
+          .then(() => {
+            console.log('Image data stored successfully')
+          })
+          .catch((error) => {
+            console.error('Error storing image data:', error)
+          })
       })
-    })
+      .catch((error) => {
+        console.error('Error uploading image:', error)
+      })
   }
 
   useEffect(() => {
@@ -39,13 +76,36 @@ function App(): JSX.Element {
     }
   }
 
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setImageName(event.target.value)
+  }
+
+  const handleAdditionalDataChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setAdditionalData(event.target.value)
+  }
+
   return (
     <div className="App">
       <input type="file" onChange={handleFileChange} />
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={imageName}
+        onChange={handleNameChange}
+      />
+      <input
+        type="text"
+        placeholder="Enter additional data"
+        value={additionalData}
+        onChange={handleAdditionalDataChange}
+      />
       <button onClick={uploadFile}>Upload Image</button>
       {imageUrls.map((url) => (
         <img key={url} src={url} alt="Uploaded" />
       ))}
+      {imageUrls}
     </div>
   )
 }
