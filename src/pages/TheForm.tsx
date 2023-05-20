4e5be0bc9051106b560319c15dbe65e638472abf
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-20 11:10:20 +0100

    commit

--- src/pages/TheForm.tsx
+++ src/pages/TheForm.tsx
@@ -8,108 +8,40 @@ import {
 import {
   getFirestore,
   collection,
-  updateDoc,
-  doc,
-  getDoc,
   addDoc,
+  onSnapshot,
+  doc,
 } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
 import { storage } from '../config/firebase'
 import { v4 as uuidv4 } from 'uuid'
 
-interface TheFormProps {
-  itemId?: string
+interface ImageData {
+  id: string
+  name: string
+  github: string
+  live: string
+  imageUrl: string
 }
 
-function TheForm({ itemId }: TheFormProps): JSX.Element {
+function TheForm(): JSX.Element {
   const [imageUpload, setImageUpload] = useState<File | null>(null)
   const [imageUrls, setImageUrls] = useState<string[]>([])
   const [name, setName] = useState<string>('')
   const [github, setGithub] = useState<string>('')
   const [live, setLive] = useState<string>('')
-  const [image, setImage] = useState<string>('')
   const navigate = useNavigate()
   const imagesListRef = storageRef(storage, 'images/')
 
   useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const db = getFirestore()
-        const itemDoc = doc(collection(db, 'portfolios'), itemId)
-        const itemSnapshot = await getDoc(itemDoc)
-
-        if (itemSnapshot.exists()) {
-          const itemData = itemSnapshot.data()
-          if (itemData) {
-            setName(itemData.name)
-            setGithub(itemData.github)
-            setLive(itemData.live)
-            setImage(itemData.imageUrl)
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-
-    if (itemId) {
-      fetchData()
-    }
-  }, [itemId])
-
-  const uploadFile = async (): Promise<void> => {
-    if (
-      imageUpload === null ||
-      name.trim() === '' ||
-      github.trim() === '' ||
-      live.trim() === ''
-    ) {
-      return
-    }
-
-    const newImageName = name + uuidv4()
-    const imageRef = storageRef(storage, `images/${newImageName}`)
-
-    try {
-      const snapshot = await uploadBytes(imageRef, imageUpload)
-      const imageUrl = await getDownloadURL(snapshot.ref)
-
-      const imageData = {
-        name: name,
-        github: github,
-        live: live,
-        imageUrl: snapshot.ref.fullPath,
-      }
-
-      const db = getFirestore()
-      const imagesCollection = collection(db, 'portfolios')
-
-      if (itemId) {
-        const itemDoc = doc(imagesCollection, itemId)
-        await updateDoc(itemDoc, imageData)
-        console.log('Item updated successfully')
-      } else {
-        await addDoc(imagesCollection, imageData)
-        console.log('Image data stored successfully')
-      }
-
-      navigate('/thepage')
-    } catch (error) {
-      console.error('Error uploading image:', error)
-    }
-  }
-
-  useEffect(() => {
-    listAll(imagesListRef)
-      .then((response) =>
-        Promise.all(response.items.map((item) => getDownloadURL(item)))
+    const fetchImages = async () => {
+      const response = await listAll(imagesListRef)
+      const urls = await Promise.all(
+        response.items.map((item) => getDownloadURL(item))
       )
-      .then((urls) => {
-        setImageUrls(urls)
-      })
-      .catch((error) => {
-        console.error('Error fetching images:', error)
-      })
+      setImageUrls(urls)
+    }
+    fetchImages()
   }, [])
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -130,10 +62,44 @@ function TheForm({ itemId }: TheFormProps): JSX.Element {
     setLive(event.target.value)
   }
 
+  const uploadFile = async (): Promise<void> => {
+    if (
+      imageUpload === null ||
+      name.trim() === '' ||
+      github.trim() === '' ||
+      live.trim() === ''
+    ) {
+      return
+    }
+
+    const newImageName = name + uuidv4()
+    const imageRef = storageRef(storage, `images/${newImageName}`)
+
+    try {
+      const snapshot = await uploadBytes(imageRef, imageUpload)
+      const imageUrl = await getDownloadURL(snapshot.ref)
+
+      const imageData: ImageData = {
+        id: uuidv4(),
+        name: name,
+        github: github,
+        live: live,
+        imageUrl: snapshot.ref.fullPath,
+      }
+
+      const db = getFirestore()
+      const imagesCollection = collection(db, 'portfolios')
+
+      await addDoc(imagesCollection, imageData)
+      console.log('Image data stored successfully')
+      navigate('/thepage')
+    } catch (error) {
+      console.error('Error uploading image:', error)
+    }
+  }
+
   return (
     <div className="App">
-      {image && <img src={image} alt="Fetched Image" />}
-
       <input type="file" onChange={handleFileChange} />
       <input
         type="text"
@@ -155,7 +121,10 @@ function TheForm({ itemId }: TheFormProps): JSX.Element {
       />
       <button onClick={uploadFile}>Upload Image</button>
       {imageUrls.map((url) => (
-        <img key={url} src={url} alt="Uploaded" />
+        <div key={url}>
+          <img src={url} alt="Uploaded" />
+          <p>{github}</p>
+        </div>
       ))}
     </div>
   )
