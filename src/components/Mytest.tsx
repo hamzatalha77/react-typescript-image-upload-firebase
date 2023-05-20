67f423c0f2de608587b8ac3d000564fa922617ad
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-21 00:26:18 +0100

    COMMMIT

--- src/components/Mytest.tsx
+++ src/components/Mytest.tsx
@@ -9,8 +9,8 @@ interface Props {
 const Mytest = ({ item, handleUpdateClick }: Props) => {
   return (
     <div>
-      <h2>{item.name}</h2>
       <img src={item.imageUrl} alt="" />
+      <h2>{item.name}</h2>
       <button onClick={item.onDelete}>Delete me!</button>
       <button onClick={() => handleUpdateClick(item)}>Update me!</button>
     </div>
