e41f0fe5f132c7e7df61a6d38e78bae38b0ad7dd
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-21 11:04:26 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', '*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
