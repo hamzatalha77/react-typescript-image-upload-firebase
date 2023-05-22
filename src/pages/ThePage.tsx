565d4a834a8b897582faa224b019f09cace9c28c
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-22 11:15:55 +0100

    commit

--- src/pages/ThePage.tsx
+++ src/pages/ThePage.tsx
@@ -221,7 +221,7 @@ const ThePage = () => {
 
         console.log('Image has been updated successfully')
       }
-
+      setSelectedFile(null)
       await fetchUpdatedData() // Fetch the updated data again
     } catch (error) {
       console.error('Error updating item:', error)
