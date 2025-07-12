c37219cbb04a457185f6d9f9ffea5031307e194e
hamzatalha77 <hamza_ta7@hotmail.com>
2025-07-12 21:38:03 +0100

    Refactors Firebase config to use environment variables
    Replaces hardcoded Firebase configuration values with environment variables for improved security and flexibility. Updates dependencies to include the latest version of `dotenv` and modifies `.gitignore` to allow `.env.local` for local development.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBrr86OuE6dPID1YZS1klQCLQ8bWtaON_I',
-  authDomain: 'something-5e33c.firebaseapp.com',
-  projectId: 'something-5e33c',
-  storageBucket: 'something-5e33c.appspot.com',
-  messagingSenderId: '886615306959',
-  appId: '1:886615306959:web:716429962ce8af37f6fdba',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const storage = getStorage(app)
 export const db = getFirestore(app)
